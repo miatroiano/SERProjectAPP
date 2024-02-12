76c665259ad0ac9dcc14cd596474a65196354b3b
miatroiano <m11ssing1n4ction@gmail.com>
2024-02-12 11:50:45 -0500

    Initial Commit

---
+++ SERProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SERProject
+//
+//  Created by Mia Troiano on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SERProject/SERProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SERProjectApp.swift
+//  SERProject
+//
+//  Created by Mia Troiano on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SERProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
